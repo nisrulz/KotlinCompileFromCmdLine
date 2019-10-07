62f1b3d01de3f97f129ad7c9a5236e0aee569cfe
Nishant Srivastava <nisrulz@gmail.com>
2019-10-08 00:03:34 +0200

    added project type 1

---
+++ project_type_1/src/com/example/cmdline/Cake.kt
@@ -0,0 +1,3 @@
+package com.example.cmdline
+
+data class Cake(val name:String)
---
+++ project_type_1/src/com/example/cmdline/Sweet.kt
@@ -0,0 +1,8 @@
+package com.example.cmdline
+
+fun main() {
+    val candy = Candy("Popsicle")
+    val cake = Cake("Blueberry Pie")
+    val juice = Juice("Apple")
+    println("I like the sweet stoff such as ${candy.name}, ${cake.name} and ${juice.name}")
+}
